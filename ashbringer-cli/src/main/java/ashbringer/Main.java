056e6427379677162fc9bd2780f65487171661da
Pavel Drankov <titantins@gmail.com>
2017-06-25 16:07:13 +0300

    cli improvements

--- ashbringer-cli/src/main/java/ashbringer/Main.java
+++ ashbringer-cli/src/main/java/ashbringer/Main.java
@@ -10,12 +10,18 @@ public class Main {
 
 
         Vertx vertx = Vertx.vertx();
-        HttpScheduler httpScheduler = HttpScheduler.create(new SchedulerOptions()
-                .setThreadCount(5)
-                .setMaxSessions(50_000)
-                .setVertx(vertx)
+        SchedulerOptions options = new SchedulerOptions()
                 .setHost(args[0])
                 .setPort(Integer.parseInt(args[1]))
+                .setVertx(vertx);
+
+        if (args.length >= 3)
+            options.setMaxSessions(Integer.parseInt(args[2]));
+
+        if (args.length >= 4)
+            options.setThreadCount(Integer.parseInt(args[3]));
+
+        HttpScheduler httpScheduler = HttpScheduler.create(options
         );
 
         httpScheduler.start();
