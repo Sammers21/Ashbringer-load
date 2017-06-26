e583f26369eadc7e870d59555ed6f14d20b20999
Pavel Drankov <titantins@gmail.com>
2017-06-26 19:36:43 +0300

    license

--- ashbringer-core/src/main/java/ashbringer/core/HttpClientInitializer.java
+++ ashbringer-core/src/main/java/ashbringer/core/HttpClientInitializer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Pavel Drankov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ashbringer.core;
 
 
@@ -26,13 +41,5 @@ public class HttpClientInitializer extends ChannelInitializer<SocketChannel> {
             p.addLast(sslCtx.newHandler(ch.alloc()));
         }
         p.addLast(new HttpClientCodec());
-
-        // Remove the following line if you don't want automatic content decompression.
-        // p.addLast(new HttpContentDecompressor());
-
-        // Uncomment the following line if you don't want to handle HttpContents.
-        //p.addLast(new HttpObjectAggregator(1048576));
-
-        //p.addLast(new HttpClientHandler());
     }
 }
